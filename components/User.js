cae881eed5543fd760efaaffb3e8bf263f403b29
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-11-11 20:24:47 -0300

    Bounce do usuário

---
+++ components/User.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react';
+
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Animated,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const styles = StyleSheet.create({
+  userContainer: {
+    marginTop: 10,
+    borderRadius: 10,
+    flexDirection: 'column',
+    marginHorizontal: 15,
+  },
+
+  thumbnail: {
+    width: '100%',
+    height: 150,
+  },
+
+  infoContainer: {
+    backgroundColor: '#57BCBC',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+  },
+
+  bioContainer: {
+    flex: 1,
+  },
+
+  name: {
+    color: '#FFF',
+    fontWeight: '900',
+    fontSize: 10,
+  },
+
+  description: {
+    color: '#FFF',
+    fontSize: 13,
+    marginTop: 2,
+  },
+
+  likesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    paddingVertical: 3,
+    paddingHorizontal: 8,
+    borderRadius: 20,
+  },
+
+  likes: {
+    color: '#FFF',
+    fontSize: 12,
+    marginLeft: 5,
+  },
+});
+
+const User = ({ user, onPress }) => {
+  const [opacity, setOpacity] = useState(new Animated.Value(0));
+  const [offset, setOffSet] = useState(
+    new Animated.ValueXY({
+      x: 0,
+      y: 50,
+    })
+  );
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.spring(offset.y, {
+        toValue: 0,
+        speed: 5,
+        bounciness: 20,
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 500,
+      }),
+    ]).start();
+  }, []); // eslint-disable-line
+
+  return (
+    <Animated.View
+      style={[
+        {
+          transform: [...offset.getTranslateTransform()],
+        },
+      ]}
+    >
+      <TouchableWithoutFeedback onPress={onPress}>
+        <View style={styles.userContainer}>
+          <Image style={styles.thumbnail} source={{ uri: user.thumbnail }} />
+
+          <View style={[styles.infoContainer, { backgroundColor: user.color }]}>
+            <View style={styles.bioContainer}>
+              <Text style={styles.name}>{user.name.toUpperCase()}</Text>
+              <Text style={styles.description}>{user.description}</Text>
+            </View>
+            <View style={styles.likesContainer}>
+              <Icon name="heart" size={12} color="#FFF" />
+              <Text style={styles.likes}>{user.likes}</Text>
+            </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </Animated.View>
+  );
+};
+
+export default User;
